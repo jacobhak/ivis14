8dfef67bc3c7014e5d3a7968bfdcb09015cbb152
Jacob Håkansson <jacobhakansson@gmail.com>
2014-02-06 21:08:23 +0100

    Can load data from google spreadsheet and draw it to a table

--- 3/gapminder.js
+++ 3/gapminder.js
@@ -1,1 +1,25 @@
-var app;
+var CO2_URL = "https://spreadsheets.google.com/spreadsheet/pub?key=0ArfEDsV3bBwCcGhBd2NOQVZ1eWoxZ2tOdVVFWE9HYWc&gid=0";
+
+function main(){
+  google.load('visualization', '1', {'packages':['corechart', 'table']});
+
+  // Set a callback to run when the Google Visualization library is loaded.
+  google.setOnLoadCallback(drawChart);
+  
+}
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+  var query = new google.visualization.Query(CO2_URL);
+  query.send(handleQueryResponse);
+  // Create our data table.
+}
+
+function handleQueryResponse(response) {
+  var data = response.getDataTable();
+  var chart = new google.visualization.Table(document.getElementById('chart'));
+  chart.draw(data, null);
+}
+main();
